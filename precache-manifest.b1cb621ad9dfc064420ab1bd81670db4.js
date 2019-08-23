a979bd1b55fb7be02b94458a0198f81c4a0a291a
mukhlisadhe <adhemukhlis@gmail.com>
2019-08-23 14:37:54 +0700

    #1

---
+++ precache-manifest.b1cb621ad9dfc064420ab1bd81670db4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2ee942cdba32896d29ad06d796d4b01",
+    "url": "/static/media/marker.c2ee942c.svg"
+  },
+  {
+    "revision": "c9f91b6a57e24fa2f584f362c2aa09e8",
+    "url": "/static/media/center.c9f91b6a.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8fca514e6464e2c21ef1",
+    "url": "/static/js/main.8fca514e.chunk.js"
+  },
+  {
+    "revision": "c61f45fdf86724215116",
+    "url": "/static/js/2.c61f45fd.chunk.js"
+  },
+  {
+    "revision": "8fca514e6464e2c21ef1",
+    "url": "/static/css/main.ca697583.chunk.css"
+  },
+  {
+    "revision": "dc2eb0a66418374fa040aa2c5d46c89d",
+    "url": "/index.html"
+  }
+];
